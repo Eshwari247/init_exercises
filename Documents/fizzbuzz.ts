7d9bcc03157b3035e47e0cfbb784636d1bc8df9a
Eshwari247 <eshwari.jaladurgam.j8f@jp.denso.com>
2023-10-10 13:11:31 +0900

    typescriptfile

---
+++ Documents/fizzbuzz.ts
@@ -0,0 +1,12 @@
+let i: number;
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+        console.log(i, "Fizz");
+    }
+    else if (i % 5 === 0 && i % 3 !== 0) {
+        console.log(i, "Buzz");
+    }
+    else if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i, "FizzBuzz");
+    }
+}
