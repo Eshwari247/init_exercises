5e12d0603192bef9f0c0cbcc2ab50812af0637af
Eshwari247 <146924883+Eshwari247@users.noreply.github.com>
2023-10-25 07:09:18 +0900

    Test via Jest

---
+++ EX5/api.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('./app'); // Import your Express app
+
+describe('GET /cal', () => {
+  it('should return 0', async () => {
+    const response = await request(app).get('/cal');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ answer: '0' });
+  });
+});
+
+describe('POST /cal', () => {
+  it('should return the result of multiplication', async () => {
+    const response = await request(app)
+      .post('/cal')
+      .send({ A: 3, B: 5 });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ answer: '15' });
+  });
+});
